bf6168e9a500d7312093ee9959c894f348e0e811
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-11-05 18:05:49 +0100

    [unfeature] Stub out everything neighbor-site-discovery-ish

--- lib/pod.php
+++ lib/pod.php
@@ -28,31 +28,15 @@ class Site {
 
     static function this_site () {
         $thisclass = get_called_class();
-        list($htdocs_path, $under_htdocs) = static::_htdocs_split();
+        $htdocs_path = ".";  # TODO: repair
+        $under_htdocs = "";  # TODO: repair
         $that = new $thisclass($under_htdocs);
         $that->htdocs_path = $htdocs_path;
         return $that;
     }
 
     static function root () {
-        [ $htdocs, $my_subdirs ] = static::_htdocs_split();
-        $path_components = explode('/', $my_subdirs);
-
-        for($under_htdocs = '';
-            true;
-            $under_htdocs = ($under_htdocs ? "$under_htdocs/" : "") .
-                            array_shift($path_components))
-        {
-            if (static::exists("$htdocs/$under_htdocs")) {
-                $thisclass = get_called_class();
-                $root = new $thisclass($under_htdocs);
-                $root->htdocs_path = $htdocs;
-                return $root;
-            }
-            if (! count($path_components)) {
-                throw new \Error('Unable to find root site from ' . WP_CONTENT_DIR);
-            }
-        }
+        return static::this_site();    # TODO: repair
     }
 
     /**
@@ -63,28 +47,7 @@ class Site {
      *              you want.
      */
     static function exists ($path) {
-        if (! preg_match('#^/#', $path)) {
-            $path = static::_htdocs_split()[0] . "/$path";
-        }
-        return is_file("$path/wp-config.php");
-    }
-
-    /**
-     * @return A list of two elements, where the first is the absolute path
-     *         to the htdocs directory and the second is the relative path
-     *         from the htdocs directory to the directory containing
-     *         wp-config.php
-     */
-    static private function _htdocs_split () {
-        $wp_content_dir = preg_replace('#/+#', '/', WP_CONTENT_DIR);
-        if (! preg_match('#(^.*/htdocs)(/.*|)$#', $wp_content_dir, $matched)) {
-            throw new \Error('Unable to find htdocs in ' . $wp_content_dir);
-        }
-        $htdocs = $matched[1];
-        $below_htdocs = preg_replace('#^/#', '',
-                                     preg_replace('#/wp-content/?$#', '',
-                                                  $matched[2]));
-        return array($htdocs, $below_htdocs);
+        return FALSE;
     }
 
     function get_path () {
@@ -170,39 +133,15 @@ class Site {
      * has not a configurated root menu.
      */
     function is_main_root () {
-        return $this->is_root() && empty($this->get_configured_root_menu_url());
+        return TRUE;     # TODO: repair
     }
 
     function get_subsites () {
-        if (! $this->htdocs_path) {
-            throw new \Error("Sorry, ->get_subsites() only works on ::this_site() for now");
-        }
-
-        return static::_get_subsites_recursive (
-            $this->htdocs_path, $this->path_under_htdocs);
+        return [];     # TODO: repair
     }
 
-    /**
-     * @return The 'menu_root_provider_url' entry in epfl-wp-sites-config.ini , or NULL if no such URL is configured.
-     */
     function get_configured_root_menu_url () {
         return $this->get_pod_config('menu_root_provider_url');
-    }
-
-    static function _get_subsites_recursive ($htdocs_path, $path) {
-        $retvals = array();
-        foreach (scandir("$htdocs_path/$path") as $filename) {
-            if (preg_match('#^([.]|wp-)#', $filename)) continue;
-            $subdir = strlen($path) ? "$path/$filename" : $filename;
-            if (! is_dir("$htdocs_path/$subdir")) continue;
-            if (static::exists($subdir)) {
-                $thisclass = get_called_class();
-                $retvals[] = new $thisclass($subdir);
-            } else {
-                $retvals = array_merge($retvals, static::_get_subsites_recursive($htdocs_path, $subdir));
-            }
-        }
-        return $retvals;
     }
 
     /**
@@ -214,41 +153,6 @@ class Site {
      */
 
     static function get_pod_config ($key='') {
-        $ini_path = static::_get_wp_site_config_path();
-        if ($ini_path) {
-            $ini_array = parse_ini_file($ini_path);
-            if (!empty($key)) {
-                if (array_key_exists($key, $ini_array)){
-                    return $ini_array[$key];
-                }
-            } else {
-                return $ini_array;
-            }
-        }
-    }
-
-    static $_wp_site_config_path_cache = FALSE;
-    static function _get_wp_site_config_path () {
-        if (FALSE === static::$_wp_site_config_path_cache) {
-            [ $htdocs, $my_subdirs ] = static::_htdocs_split();
-
-            for(
-              $path_components = explode('/', $my_subdirs);
-              ;
-              array_pop($path_components))
-            {
-                $try_path = $htdocs . "/" . implode("/", $path_components) . "/epfl-wp-sites-config.ini";
-                if (file_exists($try_path)) {
-                    static::$_wp_site_config_path_cache = $try_path;
-                    break;
-                }
-                if (!count($path_components)) break;
-            }
-            if (FALSE === static::$_wp_site_config_path_cache) {
-              static::$_wp_site_config_path_cache = NULL;
-            }
-        }
-
-        return static::$_wp_site_config_path_cache;
+        return [];  # TODO: repair
     }
 }
